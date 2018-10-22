7027fd4e3012435bda5c71cdddba9c62f1017160
ganesan <ganesansays@gmail.com>
2018-10-23 01:13:23 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import { jsPlumb } from 'jsplumb';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'jsplumb-sample';
+export class AppComponent implements AfterViewInit {
+  title = 'Angular JsPlumb Integration';
+  jsPlumbInstance;
+  showConnectionToggle = false;
+  buttonName = 'Connect';
+
+  ngAfterViewInit() {
+    this.jsPlumbInstance = jsPlumb.getInstance();
+  }
+
+  showConnectOnClick() {
+    this.showConnectionToggle = ! this.showConnectionToggle;
+    if ( this.showConnectionToggle) {
+    this.buttonName = 'Dissconnect';
+      this.connectSourceToTargetUsingJSPlumb();
+    } else {
+      this.buttonName = 'Connect';
+      this.jsPlumbInstance.reset();
+    }
+  }
+
+  connectSourceToTargetUsingJSPlumb() {
+    let labelName;
+      labelName = 'connection';
+      this.jsPlumbInstance.connect({
+        connector: ['Flowchart', {stub: [212, 67], cornerRadius: 1, alwaysRespectStubs: true}],
+        source: 'Source',
+        target: 'Target1',
+        anchor: ['Right', 'Left'],
+        paintStyle: {stroke: '#456', strokeWidth: 4},
+        overlays: [
+          ['Label', {label: labelName, location: 0.5, cssClass: 'connectingConnectorLabel'}]
+        ],
+      });
+  }
 }
